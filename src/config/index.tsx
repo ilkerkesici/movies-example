499b9ea5eeb5afdc28e849e9013288509e157062
İlker Kesici <ilker.kesici@deltasmart.tech>
2020-10-19 09:48:34 +0300

    first commit

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,3 +1,3 @@
 export const BASE_URL = 'http://api.themoviedb.org/3/';
-export const API_KEY = '579c7fc7840ab037036071939351dc60';
+export const API_KEY = 'YOUR_API_KEY';
 export const IMAGE_URL = 'https://image.tmdb.org/t/p/w500/';
